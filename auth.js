9c82bf75745a6b28e03ff114059e779751af4d2d
naymersonsantossouza-dot <naymersonsantossouza@gmail.com>
2026-01-21 21:07:26 -0300

    Implement authentication functions using Supabase

---
+++ auth.js
@@ -0,0 +1,30 @@
+const SUPABASE_URL = "https://ofmtqwwnhknnjpbddduj.supabase.co";  
+const SUPABASE_ANON_KEY = "SUA_ANON_PUBLIC_KEY";  
+  
+const supabase = supabase.createClient(  
+  SUPABASE_URL,  
+  SUPABASE_ANON_KEY  
+);  
+  
+// LOGIN  
+async function login(email, password) {  
+  const { error } = await supabase.auth.signInWithPassword({  
+    email,  
+    password,  
+  });  
+  if (error) alert(error.message);  
+}  
+  
+// CADASTRO  
+async function signup(email, password) {  
+  const { error } = await supabase.auth.signUp({  
+    email,  
+    password,  
+  });  
+  if (error) alert(error.message);  
+}  
+  
+// LOGOUT  
+async function logout() {  
+  await supabase.auth.signOut();  
+}
